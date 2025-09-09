a0edb8de88efe725384c161327913ed896865d12
Maksymilian Stolarek <stolar@MacBook-Air-Maksymilian.local>
2025-09-09 13:21:41 +0200

    Dodano modele: Team i Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(User, related_name='teams')
+
+    def __str__(self):
+        return self.name
+    
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='tasks')
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
