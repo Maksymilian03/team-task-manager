a83b3c9ce25d0edc02236ab872d110bd889cf9f1
Maksymilian Stolarek <stolar@MacBook-Air-Maksymilian.local>
2025-09-09 13:30:15 +0200

    Dodano serializery: UserSerializer, TeamSerializer, TaskSerializer

---
+++ tasks/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Task, Team
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    members = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'members']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer(read_only=True)
+    team = TeamSerializer(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'assigned_to', 'team', 'due_date', 'completed']
+        
