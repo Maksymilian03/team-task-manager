2a1a51c6d8435741470cb4f007a5fd9fe4a5ae72
Maksymilian Stolarek <stolar@MacBook-Air-Maksymilian.local>
2025-09-10 13:38:22 +0200

    Dodano uprawnienia do edycji zadań — tylko przypisany użytkownik lub admin

--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Count
+from .permissions import IsAssignedOrAdmin
 
 
 
@@ -19,7 +20,7 @@ class TeamViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAssignedOrAdmin]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['completed', 'team', 'due_date', 'status']
 
