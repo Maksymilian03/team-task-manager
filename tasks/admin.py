f1424f9d7078564065c4859ad2ef1ec93750094e
Maksymilian Stolarek <maksiu2003xd@gmail.com>
2025-09-12 13:22:45 +0200

    Ustawiono panel admina

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Task, Profile, Team, Category
+
 
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'assigned_to', 'due_date', 'status', 'team']
+    list_filter = ['status', 'due_date', 'team']
+    search_fields = ['title', 'description']
+    
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role', 'team']
+    list_filter = ['role', 'team']
+    search_fields = ['user__username']
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    filter_horizontal = ['members',]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
